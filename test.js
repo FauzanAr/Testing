cf0afd10768455544a6f30014e0ca693b146510e
FauzanAr <faurinta12345@gmail.com>
2019-10-29 08:50:32 +0700

    Successfully testing with first test

--- test.js
+++ test.js
@@ -1,18 +1,46 @@
 // Importing selenium webdriver for our web driver
 var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var chromedriver = require('chromedriver');
 
-// Importing webdriver for chrome, because we using chrome
-var browser = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+// Setting path for code to opening the chrome browser
+chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
-// Inputing the url of tester website
+//  This is first test to get title of the website
+async function firstTest(){
+    try{
+        var browser = new webdriver.Builder()
+        .withCapabilities(webdriver.Capabilities.chrome())
+        .build();
 
-browser.get('http://beasiswa.polinema.ac.id/');
+        await
+        // Inputing the url of tester website
+        browser.get('http://beasiswa.polinema.ac.id/');
 
-var promise = browser.getTitle();
+        await browser.getTitle().then(function(title){
+            console.log(title)
+        });
+        browser.quit();
+    }
+    catch(err){
+        handleFailure(err, browser)
+    }
+}
 
-promise.then(function(title){
-    console.log(title);
-});
 
-// Closing the test
-browser.quit();
+// This is second test for click home feature
+
+
+
+// This running the first test
+firstTest();
+
+
+
+
+// This is error handle
+function handleFailure(err, browser){
+    console.log('Something went wrong! \n', err.stack,'\n');
+
+    browser.quit();
+}
