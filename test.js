e9d6713a6c76b36ff4cb6fd6668b278b3dadc88b
FauzanAr <faurinta12345@gmail.com>
2019-10-29 22:22:32 +0700

    Adding chai to our squad'

--- test.js
+++ test.js
@@ -2,30 +2,58 @@
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var chromedriver = require('chromedriver');
+var test = require('selenium-webdriver/testing');
 
 // Setting path for code to opening the chrome browser
 chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
-//  This is first test to get title of the website
-async function firstTest(){
-    try{
-        var browser = new webdriver.Builder()
-        .withCapabilities(webdriver.Capabilities.chrome())
-        .build();
+var browser = new webdriver.Builder()
+            .withCapabilities(webdriver.Capabilities.chrome())
+            .build();
 
-        await
-        // Inputing the url of tester website
+test.describe('First Test', function(){
+    test.before(function(){
         browser.get('http://beasiswa.polinema.ac.id/');
+    });
 
-        await browser.getTitle().then(function(title){
-            expect(title).to.equal('Fauzan');
-        });
-    }
-    // Catching the fail try of the function
-    catch(err){
-        handleFailure(err, browser)
-    }
-}
+    test.after(function(){
+        browser.quit();
+    });
+
+    test.it('The Test Case', function(){
+        browser.getTitle().then(function(){
+            expect(title).equals('fauzan');
+        })
+
+        browser.sleep();
+    });
+});
+
+
+
+//-----------------------------=====-------------------------------------------------====----------------
+
+//  This is first test to get title of the website
+// async function firstTest(){
+//     try{
+//         var browser = new webdriver.Builder()
+//         .withCapabilities(webdriver.Capabilities.chrome())
+//         .build();
+
+//         await
+//         // Inputing the url of tester website
+//         browser.get('http://beasiswa.polinema.ac.id/');
+
+//         await browser.getTitle().then(function(title){
+//             expect(title).to.equal('Fauzan');
+//         });
+//         browser.quit();
+//     }
+//     // Catching the fail try of the function
+//     catch(err){
+//         handleFailure(err, browser)
+//     }
+// }
 
 
 // This is second test for click home feature
@@ -47,12 +75,12 @@ async function firstTest(){
 
 
 // This running the first test
-firstTest();
+// firstTest();
 
 
 
 // This is error handle
-function handleFailure(err, browser){
-    console.log('Something went wrong! \n', err.stack,'\n');
-    browser.quit();
-}
+// function handleFailure(err, browser){
+//     console.log('Something went wrong! \n', err.stack,'\n');
+//     browser.quit();
+// }
